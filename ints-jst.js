405882bb443b53fc7b270fc05c96c8530cfb6cab
int soumen <Redcof@users.noreply.github.com>
2018-11-11 13:35:06 +0530

    Add files via upload
    Pure javascript. Very optimized parsing.

--- ints-jst.js
+++ ints-jst.js
@@ -1,97 +1,76 @@
-// Create a class for the element
-class jst_ints extends HTMLElement {
+"use strict";
+
+function log() {
+    let l = arguments.length;
+    for (let c = 0; c < l; c++) {
+        console.log(arguments[c]);
+    }
+}
+
+let ___TGT___ = document;
+
+function print() {
+    let l = arguments.length;
+    for (let c = 0; c < l; c++) {
+        ___TGT___.innerText += (arguments[c]);
+    }
+}
+
+
+class ___jst_element___ extends HTMLElement {
     constructor() {
-        // Always call super first in constructor
         super();
     }
 }
 
-customElements.define("js-t", jst_ints);
+const ___NAME___ = "js-t";
+const __VERSION___ = "2.1.0";
+const ___STG___ = "<" + ___NAME___ + ">";
+const ___CSTG___ = "</" + ___NAME___ + ">";
+log("Ints JST v" + __VERSION___);
+customElements.define(___NAME___, ___jst_element___);
 
-$(document).ready(function () {
-    var body = $('body');
-    const html = ($(body).html()).trim();
-    const len = html.length;
-    //const code = html.split;
-    var ctr = 0, jst_scope = false,
-        executable = '', char_ex = '', last_ch = "-9999",
-        print_buffer = '', char_pr = '', temp_pr = '', temp_ex = '';
-    const SCOPE_START = "<js-t>";
-    const SCOPE_END = "</js-t>";
-    var ___ctr_sc_start___ = 0;
-    var ___ctr_sc_end___ = 0;
+function run() {
+    let target;
+    if (arguments.length === 0) {
+        target = document.getElementsByTagName('body')[0];
+    } else {
+        target = arguments[0];
+    }
+    let tgt = target;
+    ___TGT___ = tgt;
+    let html = (tgt.innerHTML).trim();
+    let code_points = html.split(___STG___);
+    let execute = eval;
+    let printable = '', executable = '', exe = '';
 
+    function append_print(statement) {
+        statement = statement.replace(/[\n\r]/, "\\n");
+        executable += "print('" + statement + "');";
+    }
 
+    printable = code_points[0].trim();
+    append_print(printable);
 
-    for (; ctr < len;) {
-        //greed parsing scope start <jst>
-
-        char_pr = html[ctr];
-        if (!jst_scope && char_pr.toLocaleLowerCase() === SCOPE_START[___ctr_sc_start___]) {
-            ctr++;
-            ___ctr_sc_start___++;
-            temp_pr += char_pr;
-            if (___ctr_sc_start___ < SCOPE_START.length) {
-                continue;
-            }
-            ___ctr_sc_start___ = 0;
-
-            //todo scope start
-            char_pr = '';
-            temp_pr = '';
-            jst_scope = true;
-        } else if (jst_scope) {
-            //flush the print buffer
-            executable += "print('" + print_buffer + "');";
-            print_buffer = '';
-
-            for (; ctr < len;) {
-                //search for scope end
-                char_ex = html[ctr];
-                if (jst_scope && char_ex.toLocaleLowerCase() === SCOPE_END[___ctr_sc_end___]) {
-                    ctr++;
-                    ___ctr_sc_end___++;
-                    temp_ex += char_ex;
-                    if (___ctr_sc_end___ < SCOPE_END.length) {
-                        continue;
-                    }
-
-                    //todo scope end
-                    ___ctr_sc_end___ = 0;
-                    jst_scope = false;
-                    char_ex = '';
-                    temp_ex = '';
-                }
-
-                if (char_ex === "\n" || char_ex === '\r') {
-                    char_ex = " ";
-                }
-                executable += temp_ex + char_ex;
-                ctr++;
-
-                //todo exit from scope
-                if (jst_scope === false) {
-                    ctr--;
-                    break;
-                }
-            }//for loop - run in scope
-        } else {
-            //not in scope, add to print buffer
-            if (char_pr === "\n" || char_pr === '\r') {
-                char_pr = "\\n";
-            }
-            print_buffer += temp_pr + char_pr;
-            ctr++;
+    let cp_len = code_points.length, cp_ctr;
+    for (cp_ctr = 1; cp_ctr < cp_len; cp_ctr++) {
+        let codes = code_points[cp_ctr];
+        codes = codes.trim();
+        let parts = codes.split(___CSTG___);
+        exe = parts[0].trim();
+        exe = exe.replace(/[\n\r]/, "\\");
+        exe = exe.replace(/&gt;/, ">");
+        exe = exe.replace(/&lt;/, "<");
+        executable += exe;
+        if (parts.length > 1 && parts[1].trim().length > 0) {
+            printable = parts[1].trim();
+            append_print(printable);
         }
-    }//for-loop run in code
-    $(body).html('');
-    eval(executable);
-});
-
-function log(l) {
-    console.log(l)
+    }
+    tgt.innerHTML = '';
+    execute("(function(){" + executable + "}());");
+    tgt.innerHTML = tgt.innerText;
 }
 
-function print(g) {
-    $('body').append(g);
-}
+
+
